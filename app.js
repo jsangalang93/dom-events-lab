203a77c051542e6a5cf4c804bf164ad2fc3e8d87
jsangalang93 <jsangalang93@gmail.com>
2024-04-04 16:12:09 -0400

    Initial Commit

---
+++ app.js
@@ -0,0 +1,23 @@
+/*-------------------------------- Constants --------------------------------*/
+const button1Element
+const button2Element
+const button3Element
+const button4Element
+const button5Element
+const button6Element
+const button7Element
+const button8Element
+const button9Element
+const equalsButton
+const minusButton
+const timesButton
+const divideButton
+const clearButton
+
+/*-------------------------------- Variables --------------------------------*/
+
+/*------------------------ Cached Element References ------------------------*/
+
+/*----------------------------- Event Listeners -----------------------------*/
+
+/*-------------------------------- Functions --------------------------------*/
